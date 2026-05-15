2bb233603908e612a0fbc204ed6fdac177d3c181
Andrei Chindris <andrei.chindris05@e-uvt.ro>
2026-05-15 18:36:27 +0300

    logo

--- vite.config.js
+++ vite.config.js
@@ -28,12 +28,6 @@ export default defineConfig({
             src: 'logo.png',
             sizes: '512x512',
             type: 'image/png'
-          },
-          {
-            src: 'logo.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
           }
         ]
       },
